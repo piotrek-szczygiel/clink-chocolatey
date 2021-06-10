555c3a4a1f9dea4f4e5fe5da09510f867ee9040e
Piotr Szczygieł <szczygiel.p@gmail.com>
2021-06-10 15:46:08 +0200

    Fix typo

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -3,7 +3,7 @@ $tools = Split-Path $MyInvocation.MyCommand.Definition
 . $tools\uninstall.ps1
 
 Uninstall-ChocolateyPackage `
-    -PackageName 'clink' `
+    -PackageName 'clink-maintained' `
     -File (Get-Uninstaller -Name 'Clink v1.2.12.4a4e22') `
     -FileType 'EXE' `
     -SilentArgs '/S'
