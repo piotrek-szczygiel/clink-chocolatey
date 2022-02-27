f1abbaff1d006df3824419a81bf5856bfc8bd27a
Piotr Szczygieł <szczygiel.p@gmail.com>
2022-02-27 20:35:03 +0100

    Update to 1.3.8
    Disable silent install, because autorun is disabled now by default,
    which may confuse people.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,6 @@
 ﻿Install-ChocolateyPackage `
     -PackageName 'clink-maintained' `
-    -Url 'https://github.com/chrisant996/clink/releases/download/v1.3.3/clink.1.3.3.d46fcb_setup.exe' `
-    -Checksum '9855472F425FC585B46E9FD30112EE93EDEB8DA1879511AE22FC2CAEC07E68EA' `
+    -Url 'https://github.com/chrisant996/clink/releases/download/v1.3.8/clink.1.3.8.c918a0_setup.exe' `
+    -Checksum 'BF8EAB8333ACD52AA7DDC2AB241634D31F90885C61DB308EBA9C4C565997B69C' `
     -ChecksumType 'SHA256' `
     -FileType 'EXE' `
-    -SilentArgs '/S'
