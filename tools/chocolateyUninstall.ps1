491a53ba61e946f4c98dc5440ac4c49827c4b61b
Piotr Szczygieł <szczygiel.p@gmail.com>
2022-02-27 20:15:40 +0100

    Remove icon and uninstallation scripts (use auto-uninstaller)

--- tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-$tools = Split-Path $MyInvocation.MyCommand.Definition
-
-. $tools\uninstall.ps1
-
-Uninstall-ChocolateyPackage `
-    -PackageName 'clink-maintained' `
-    -File (Get-Uninstaller -Name 'Clink v1.3.3.d46fcb') `
-    -FileType 'EXE' `
-    -SilentArgs '/S'
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿Install-ChocolateyPackage `
-    -PackageName 'clink' `
+    -PackageName 'clink-maintained' `
     -Url 'https://github.com/chrisant996/clink/releases/download/v1.3.3/clink.1.3.3.d46fcb_setup.exe' `
     -Checksum '9855472F425FC585B46E9FD30112EE93EDEB8DA1879511AE22FC2CAEC07E68EA' `
     -ChecksumType 'SHA256' `
