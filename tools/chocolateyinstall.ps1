2b33a6123825a5a462d0f6ef0b431dfb8c3fccb2
Luciano Paciornick <724969+cobrabr@users.noreply.github.com>
2022-07-12 14:17:36 -0400

    Update chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿Install-ChocolateyPackage `
     -PackageName 'clink-maintained' `
-    -Url 'https://github.com/chrisant996/clink/releases/download/v1.3.13/clink.1.3.13.24b02e_setup.exe' `
-    -Checksum 'A471BAA41B1BAA77496E192D1710FE3EF39743E55A524724C03F6AE0DD514872' `
+    -Url 'https://github.com/chrisant996/clink/releases/download/v1.3.36/clink.1.3.36.32d0bc_setup.exe' `
+    -Checksum '04596764446B0227C6FCE31E0E8A9FA5837354A9172ED91D3BA11CC03153CC9B' `
     -ChecksumType 'SHA256' `
     -FileType 'EXE' `
     -SilentArgs '/S'
