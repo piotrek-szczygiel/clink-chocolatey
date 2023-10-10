b0164ddcd25dd3a728e7abd7bf8208b18779866e
Piotr Szczygieł <szczygiel.p@gmail.com>
2023-10-10 18:37:43 +0200

    Update to 1.5.9

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿Install-ChocolateyPackage `
     -PackageName 'clink-maintained' `
-    -Url 'https://github.com/chrisant996/clink/releases/download/v1.4.21/clink.1.4.21.b59b76_setup.exe' `
-    -Checksum '986d8330e0023c15c8602ca07400db9e8408794b19955a9f965a7b5400e4a5ae' `
+    -Url 'https://github.com/chrisant996/clink/releases/download/v1.5.9/clink.1.5.9.411d0f_setup.exe' `
+    -Checksum '7e3e5c40e7ee3dbe6f3435683ec0cff629178c90c324d61ccfd05e3f4c610feb' `
     -ChecksumType 'SHA256' `
     -FileType 'EXE' `
     -SilentArgs '/S'
