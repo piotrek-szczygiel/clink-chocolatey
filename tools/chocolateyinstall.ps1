f7cbb0872f1770c3f75e20cc2c1b0031956a7f14
Laura <56841927+0x47726579@users.noreply.github.com>
2024-10-25 13:27:24 +0200

    Update chocolateyinstall.ps1 to reflect latest available version
     - URL updated
     - SHA2 updated

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿Install-ChocolateyPackage `
     -PackageName 'clink-maintained' `
-    -Url 'https://github.com/chrisant996/clink/releases/download/v1.6.10/clink.1.6.10.d5dce0_setup.exe' `
-    -Checksum '5347a59a53c4c1310e6c71d72a7a3cbbf1004f2ae9b82de90a29eda0ac36ad58' `
+    -Url 'https://github.com/chrisant996/clink/releases/download/v1.7.3/clink.1.7.3.f8fb96_setup.exe' `
+    -Checksum '67e7a6c39d48fdf302a3002e816b63e8507941a7252a886d68e07779dafc73f7' `
     -ChecksumType 'SHA256' `
     -FileType 'EXE' `
     -SilentArgs '/S'
