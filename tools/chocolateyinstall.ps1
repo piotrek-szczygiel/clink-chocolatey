9364082725699dae4ab0ae944803e537feb0c77f
Laura <56841927+0x47726579@users.noreply.github.com>
2025-01-12 10:03:39 +0100

    Update chocolateyinstall.ps1 to use clink v1.7.7

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿Install-ChocolateyPackage `
     -PackageName 'clink-maintained' `
-    -Url 'https://github.com/chrisant996/clink/releases/download/v1.7.3/clink.1.7.3.f8fb96_setup.exe' `
-    -Checksum '67e7a6c39d48fdf302a3002e816b63e8507941a7252a886d68e07779dafc73f7' `
+    -Url 'https://github.com/chrisant996/clink/releases/download/v1.7.7/clink.1.7.7.521fa7_setup.exe' `
+    -Checksum 'ad35745fee134f26fee83d9b890141484f74c9506fca88c2f3fcbe029b2e7eb6' `
     -ChecksumType 'SHA256' `
     -FileType 'EXE' `
     -SilentArgs '/S'
