17fc7b87ea47d5d88fa23d786caa2a3dd138d2b5
PyHedgehog <pyhedgehog@users.noreply.github.com>
2025-05-31 15:51:53 +0300

    Update to clink v1.7.19 (#13)

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿Install-ChocolateyPackage `
     -PackageName 'clink-maintained' `
-    -Url 'https://github.com/chrisant996/clink/releases/download/v1.7.18/clink.1.7.18.5581bd_setup.exe' `
-    -Checksum 'fb32589d41aadd761269536a062ce0fde6e52511368abaa6c17e624d55833949' `
+    -Url 'https://github.com/chrisant996/clink/releases/download/v1.7.19/clink.1.7.19.d8a218_setup.exe' `
+    -Checksum '09c62c9085e8cfec420b3459c526433bdea70c436568ab2681257995ab0089db' `
     -ChecksumType 'SHA256' `
     -FileType 'EXE' `
     -SilentArgs '/S'
